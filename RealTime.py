7c89652e4ddafe27a9a580dadb4d5343c9dcb398
Zhuohang <zhuohanglicn@163.com>
2018-03-01 02:01:27 -0500

    alpha_vantage

---
+++ RealTime.py
@@ -0,0 +1,16 @@
+from alpha_vantage.timeseries import TimeSeries
+from pprint import pprint
+import time
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+stockSymbol=sys.argv[1]
+
+while(True):
+    ts = TimeSeries(key='YOUR_API_KEY', output_format='pandas')
+    data, meta_data = ts.get_intraday(symbol=stockSymbol, interval='1min', outputsize='full')
+    pprint(data.tail(1))
+    engine = create_engine('mysql+mysqldb://root:password@localhost:3306/databasename?charset=utf8')
+    pd.io.sql.to_sql(data.tail(1), 'tablename', engine, schema='databasename', if_exists='append')
+    time.sleep(60)
