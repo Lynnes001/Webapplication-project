bada151f3e78bf63bd8677346e7c6c41d371ce94
Zhuohang <zhuohanglicn@163.com>
2018-03-01 02:09:28 -0500

    1

--- RealTime.py
+++ RealTime.py
@@ -8,9 +8,10 @@ from sqlalchemy import create_engine
 stockSymbol=sys.argv[1]
 
 while(True):
-    ts = TimeSeries(key='YOUR_API_KEY', output_format='pandas')
+    ts = TimeSeries(key='YOUR_API_KEY', output_format='pandas') #replace YOUR_API_KEY with your key
     data, meta_data = ts.get_intraday(symbol=stockSymbol, interval='1min', outputsize='full')
     pprint(data.tail(1))
-    engine = create_engine('mysql+mysqldb://root:password@localhost:3306/databasename?charset=utf8')
-    pd.io.sql.to_sql(data.tail(1), 'tablename', engine, schema='databasename', if_exists='append')
+    # save to mysql
+    # engine = create_engine('mysql+mysqldb://root:password@localhost:3306/databasename?charset=utf8')
+    # pd.io.sql.to_sql(data.tail(1), 'tablename', engine, schema='databasename', if_exists='append')
     time.sleep(60)
