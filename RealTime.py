db88b7a2912b69e7e01bbbcfc671f3a807b1abb7
Zhuohang <zhuohanglicn@163.com>
2018-03-01 16:09:12 -0500

    REALTIME data

--- RealTime.py
+++ RealTime.py
@@ -5,13 +5,19 @@ import sys
 import pandas as pd
 from sqlalchemy import create_engine
 
-stockSymbol=sys.argv[1]
+# stockSymbol=sys.argv[1]
+# while(True):
+#     ts = TimeSeries(key='0345Y49CIQ03XJK3', output_format='pandas') #replace YOUR_API_KEY with your key
+#     data, meta_data = ts.get_intraday(symbol=stockSymbol, interval='1min', outputsize='full')
+#     pprint(data.tail(1))
+#     # save to mysql
+#     # engine = create_engine('mysql+mysqldb://root:password@localhost:3306/databasename?charset=utf8')
+#     # pd.io.sql.to_sql(data.tail(1), 'tablename', engine, schema='databasename', if_exists='append')
+#     time.sleep(60)
 
-while(True):
-    ts = TimeSeries(key='YOUR_API_KEY', output_format='pandas') #replace YOUR_API_KEY with your key
+for stockSymbol in sys.argv[1:]:
+    ts = TimeSeries(key='0345Y49CIQ03XJK3', output_format='pandas')  # replace YOUR_API_KEY with your key
     data, meta_data = ts.get_intraday(symbol=stockSymbol, interval='1min', outputsize='full')
+    print('Processing '+stockSymbol)
     pprint(data.tail(1))
-    # save to mysql
-    # engine = create_engine('mysql+mysqldb://root:password@localhost:3306/databasename?charset=utf8')
-    # pd.io.sql.to_sql(data.tail(1), 'tablename', engine, schema='databasename', if_exists='append')
-    time.sleep(60)
+    data.to_csv(stockSymbol + '.txt', index=True, sep=' ')
