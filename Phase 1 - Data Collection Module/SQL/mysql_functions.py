ff44e8816982a001a805f22a882321bf8a4e2e63
Xin Yang <yx654402886@gmail.com>
2018-03-02 04:38:07 -0500

    Historical

--- Phase 1 - Data Collection Module/SQL/mysql_functions.py
+++ Phase 1 - Data Collection Module/SQL/mysql_functions.py
@@ -1,17 +1,23 @@
 import MySQLdb
-
-def connectDB():
-	# change and customize your database setting here!
-	# connect(host, dbusername, dbpasswd, dbname)
-	db = MySQLdb.connect("127.0.0.1", "root", "", "stockDB")
-	return db
+def dbInsertStock(sy):
+	#database connection
+	db = MySQLdb.connect("127.0.0.1", "kora", "1234", "stockDB")
+	cursor = db.cursor()
+	#define sql
+	sql = """INSERT INTO stock(symbol) VALUES('"""+sy+"""')"""
+	try:
+		cursor.execute(sql)
+		db.commit()
+	except:
+		db.rollback()
+	db.close()
 
 def dbInsertRealTime(list):
 	'''
 	list = [symbol, price, stock_time, volume]
 	'''
 	#database connection
-	db = connectDB()
+	db = MySQLdb.connect("127.0.0.1", "root", "ys0101", "stockDB")
 	cursor = db.cursor()
 	#define sql
 	sql = """INSERT INTO Real_Time_Data(symbol, price, stock_time, volume) VALUES('"""+list[0]+"""', """+ str(list[1]) + """, '"""+list[2]+ """', """ + str(list[3])+ """)"""
@@ -23,19 +29,13 @@ def dbInsertRealTime(list):
 	db.close()
 
 def dbInsertHistoryTime(list):
-	'''
-	list = [symbol, stock_date, open_pri, high_pri, low_pri, close_pri, adj_close, volume ]
-	'''
-	#database connection
-	db = connectDB()
+	db = MySQLdb.connect("127.0.0.1", "root", "passwd", "stockDB")
 	cursor = db.cursor()
-	#define sql
-	sql = """INSERT INTO History_Time_Data(symbol, stock_date, open_price, high_price, low_price, close_price, adj_close, volume) 
-			VALUES('"""+list[0]+"""', '"""+ str(list[1]) + """', """+list[2]+ """, """ + str(list[3])+ ""","""+list[4]+ ""","""+list[5]+ ""","""+list[6]+ ""","""+list[7]+ """)"""
+	sql = "INSERT INTO History_Time_Data(symbol, stock_date, open_price, high_price, low_price, close_price, adj_close, volume) VALUES(" + "'"+list[0] + "'," + "'" + list[1] + "'" + "," + list[2] + "," + list[3] + "," + list[4] + "," + list[5] + "," + list[6] + "," + list[7] + ")" + ";"
 	try:
 		cursor.execute(sql)
 		db.commit()
 	except:
 		db.rollback()
+		print("Insert historical data: " + list + " Failed!")
 	db.close()
-
--- Phase 1 - Data Collection Module/Xin_Yang_Historical_Python/Xin_Yang_Historical_Data.py
+++ Phase 1 - Data Collection Module/Xin_Yang_Historical_Python/Xin_Yang_Historical_Data.py
@@ -1,11 +1,22 @@
 
 # coding: utf-8
 
+# In[154]:
+
+
+import sys
+sys.path.append('../SQL')
 from urllib import urlopen
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import numpy as np
 import time
+import datetime
+import MySQLdb
+
+
+# In[155]:
+
 
 # call selenium and chromedriver to get driver
 def driveChrome(url):
@@ -23,8 +34,12 @@ def driveChrome(url):
     time.sleep(0.1)
     driver.execute_script("window.scrollBy(0,5000)")
     time.sleep(0.1)
-
+    
     return driver
+
+
+# In[156]:
+
 
 # resolve html to soup
 def getSoup(url, driver):
@@ -33,15 +48,79 @@ def getSoup(url, driver):
     soup = BeautifulSoup(driver.page_source, "lxml")
     return soup;
 
-def getHistoricalData(url):
+
+# In[157]:
+
+
+# transfer month into number
+def transMon(day):
+    monf = day.text.split(" "); # monf[0] - Jan - Dec    
+    #Jan 04, 2018
+    mon = "00"
+    if monf[0] == "Jan":
+        mon = "01"
+    elif monf[0] == "Feb":
+        mon = "02"
+    elif monf[0] == "Mar":
+        mon = "03"            
+    elif monf[0] == "Apr":
+        mon = "04"            
+    elif monf[0] == "May":
+        mon = "05"            
+    elif monf[0] == "Jun":
+        mon = "06"            
+    elif monf[0] == "Jul":
+        mon = "07"            
+    elif monf[0] == "Aug":
+        mon = "08"
+    elif monf[0] == "Sep":
+        mon = "09"        
+    elif monf[0] == "Oct":
+        mon = "10"
+    elif monf[0] == "Nov":
+        mon = "11"            
+    elif monf[0] == "Dec":
+        mon = "12"     
+        
+    dayf = monf[1].strip(',')
+    resdate = monf[2]+"-"+mon+"-"+dayf
+    return resdate  
+
+
+# In[158]:
+
+
+def getHistoricalData(stockCode):
+    url = getURL(stockCode)
     driver  = driveChrome(url)
     soup = getSoup(url,driver)
+    # quit selenium driver
+    driver.quit()
+    
     # get dates
     days = soup.find_all("td", class_ = "Py(10px) Ta(start) Pend(10px)") # <td class="Py(10px) Ta(start) Pend(10px)"><span>Feb 28, 2018</span></td>
-    dates = []
+    dates = []        
     for day in days:
-        dates.append(day.text)
+        day = transMon(day)
+        #print(day)
+        dates.append(day)
+    
+    
+    # delete invalid date
+    bad = []
+    tr = soup.find_all("tr", class_ = "BdT Bdc($c-fuji-grey-c) Ta(end) Fz(s) Whs(nw)")
 
+    for inner in tr:
+        badi = inner.find("td", class_ = "Ta(c) Py(10px) Pstart(10px)")
+        if badi:
+            #print(bad.text)
+            badparent = badi.find_parent()
+            baddate = badparent.find("td", class_ = "Py(10px) Ta(start) Pend(10px)")
+            bad.append(transMon(baddate))
+
+    for baditem in bad:
+        dates.remove(baditem)
+    
     # get prices and volume
     elements = soup.find_all("td", class_ = "Py(10px) Pstart(10px)") # <td class="Py(10px) Pstart(10px)"><span>1,123.03</span></td>
 
@@ -51,7 +130,8 @@ def getHistoricalData(url):
     closep = [] # close price
     adjclosep = [] # adjusted close price
     volume = [] # volume
-
+    
+    
     counter = 0
     for element in elements:
         #print(element.text)
@@ -69,19 +149,56 @@ def getHistoricalData(url):
             adjclosep.append(float(text))
         elif counter % 6 == 0:
             volume.append(int(text))
-    tmp = [dates, openp, highp, lowp, closep, adjclosep, volume]
+     
+    # generate symbol column
+    num = len(volume)
+    symbols = []
+    for i in range(0,num):
+        symbols.append(stockCode)        
+    
+    print(len(symbols), len(dates), len(openp), len(highp), len(lowp), len(closep), len(adjclosep), len(volume))
+    tmp = [symbols,dates, openp, highp, lowp, closep, adjclosep, volume]
     res = np.stack(tmp,axis = 1)
-
+      
+    # insert into database
+    for record in res:
+        dbInsertHistoryTime(record)
+    
     return res
 
+
+# In[159]:
+
+
+# generate Yahoo Finance url
 def getURL(stockCode):
     return "https://finance.yahoo.com/quote/"+ stockCode + "/history?p=" + stockCode
 
 
+# In[160]:
+
+
+# insert data into database
+def dbInsertHistoryTime(list):
+    db = MySQLdb.connect("127.0.0.1", "root", "yx960203", "stockDB") # Change your database info here!
+    cursor = db.cursor()
+    sql = "INSERT INTO History_Time_Data(symbol, stock_date, open_price, high_price, low_price, close_price, adj_close, volume) VALUES(" + "'"+list[0] + "'," + "'" + list[1] + "'" + "," + list[2] + "," + list[3] + "," + list[4] + "," + list[5] + "," + list[6] + "," + list[7] + ")" + ";"
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except:
+        db.rollback()
+        print("Insert historical data Failed!")
+    db.close()
+
+
 # ------------------------------- ** Examples ** --------------------------------------
 
+# In[161]:
+
+
 """
-Example: example = getHistoricalData(getURL("yourStockCode"))
+Example: example = getHistoricalData("yourStockCode")
 
 
 example: records of all year
@@ -102,3 +219,64 @@ Tested Feb.28.2018 by Xin Yang.
 
 *Close price adjusted for splits. **Adjusted close price adjusted for both dividends and splits.
 """
+
+
+# In[162]:
+
+
+getHistoricalData("GOOG")    
+
+
+# In[163]:
+
+
+getHistoricalData("AABA")
+
+
+# In[164]:
+
+
+getHistoricalData("CSCO")
+
+
+# In[165]:
+
+
+getHistoricalData("T")
+
+
+# In[166]:
+
+
+getHistoricalData("WMT")
+
+
+# In[167]:
+
+
+getHistoricalData("NOK")
+
+
+# In[168]:
+
+
+getHistoricalData("NFLX")
+
+
+# In[169]:
+
+
+getHistoricalData("APA")
+
+
+# In[170]:
+
+
+getHistoricalData("NKE")
+
+
+# In[171]:
+
+
+getHistoricalData("GE")
+
