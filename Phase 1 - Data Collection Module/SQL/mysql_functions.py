09e9e9a7760bc92cb04ce9f108226a9335695c26
Kora <20915951+KoraXia@users.noreply.github.com>
2018-03-01 19:10:53 -0500

    Update mysql_functions.py

--- Phase 1 - Data Collection Module/SQL/mysql_functions.py
+++ Phase 1 - Data Collection Module/SQL/mysql_functions.py
@@ -21,8 +21,6 @@ def dbInsertRealTime(list):
 	db = MySQLdb.connect("127.0.0.1", "root", "", "stockDB")
 	cursor = db.cursor()
 	#define sql
-	sql = """INSERT INTO Real_Time_Data(symbol, price, stock_time, volume) 
-			VALUES(list[0], list[1], list[2], list[3])"""
 	sql = """INSERT INTO Real_Time_Data(symbol, price, stock_time, volume) VALUES('"""+list[0]+"""', """+ str(list[1]) + """, '"""+list[2]+ """', """ + str(list[3])+ """)"""
 	print sql
 	try:
@@ -41,7 +39,7 @@ def dbInsertHistoryTime(list):
 	cursor = db.cursor()
 	#define sql
 	sql = """INSERT INTO History_Time_Data(symbol, stock_date, open_price, high_price, low_price, close_price, adj_close, volume) 
-			VALUES(list[0], list[1], list[2], list[3], list[4], list[5], list[6], list[7])"""
+			VALUES('"""+list[0]+"""', '"""+ str(list[1]) + """', """+list[2]+ """, """ + str(list[3])+ ""","""+list[4]+ ""","""+list[5]+ ""","""+list[6]+ ""","""+list[7]+ """)"""
 	try:
 		cursor.execute(sql)
 		db.commit()
