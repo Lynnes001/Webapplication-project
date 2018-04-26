80663fe9b0e8240c28df1ebbbd57f28238b7f772
Zhuohang <zhuohanglicn@163.com>
2018-04-26 01:16:43 -0400

    api

--- Phase 1 - Data Collection Module/Zhuohang_Li_API_Python/API.py
+++ Phase 1 - Data Collection Module/Zhuohang_Li_API_Python/API.py
@@ -13,3 +13,53 @@ def get_latest(symbol):
     c=[b[0][0],b[0][1],b[0][2],b[0][3],b[0][4]]
     a.extend(c)
     return a
+
+
+""" return the data within last 10 days(default)/1 year that has the highest 'high' value,
+    with the format of: [timestamp, open, high, low, close, volume]
+"""
+def get_high(symbol, period='10days'):
+    ts = TimeSeries(key='0345Y49CIQ03XJK3', output_format='pandas')
+    data, meta_data = ts.get_daily(symbol=symbol, outputsize='full')
+    if(period=='10days'):
+        slice=data.tail(10)
+    if(period=='1year'):
+        slice=data.tail(365)
+    sorted=slice.sort_values(by='2. high')
+    a=list(np.array(np.array(sorted.tail(1).index)))
+    b=np.array(sorted.tail(1)).tolist()
+    c=[b[0][0],b[0][1],b[0][2],b[0][3],b[0][4]]
+    a.extend(c)
+    return a
+
+
+""" return the data within last 10 days(default)/1 year that has the lowest 'low' value,
+    with the format of: [timestamp, open, high, low, close, volume]
+"""
+def get_low(symbol, period='10days'):
+    ts = TimeSeries(key='0345Y49CIQ03XJK3', output_format='pandas')
+    data, meta_data = ts.get_daily(symbol=symbol, outputsize='full')
+    if(period=='10days'):
+        slice=data.tail(10)
+    if(period=='1year'):
+        slice=data.tail(365)
+    sorted=slice.sort_values(by='3. low')
+    a=list(np.array(np.array(sorted.head(1).index)))
+    b=np.array(sorted.head(1)).tolist()
+    c=[b[0][0],b[0][1],b[0][2],b[0][3],b[0][4]]
+    a.extend(c)
+    return a
+
+
+""" return average close value of last 10 days(default)/1 year
+"""
+def get_avg(symbol, period='10days'):
+    ts = TimeSeries(key='0345Y49CIQ03XJK3', output_format='pandas')
+    data, meta_data = ts.get_daily(symbol=symbol, outputsize='full')
+    if(period=='10days'):
+        slice=data.tail(10)
+    if(period=='1year'):
+        slice=data.tail(365)
+    print(slice)
+    close=slice['4. close']
+    return close.mean()
