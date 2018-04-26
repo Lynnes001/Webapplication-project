37c2b1df4889e138d000823885844359817a6794
Xin Yang <yx654402886@gmail.com>
2018-04-26 05:25:49 -0400

    realtime_updated

--- Phase 1 - Data Collection Module/Song_Yang_Realtime_Python/Song_Yang_Realtime_Data.py
+++ Phase 1 - Data Collection Module/Song_Yang_Realtime_Python/Song_Yang_Realtime_Data.py
@@ -2,6 +2,7 @@
 import datetime
 import requests
 import json
+import sys
 
 def getRealtimeStock(symbol):
     url=getURL(symbol)
@@ -15,38 +16,38 @@ def getRealtimeStock(symbol):
     i1=r.find('{', i1)
     i2=r.find("\n", i1)
     i2=r.rfind(';', i1, i2)
-    jsonstr=r[i1:i2]      
+    jsonstr=r[i1:i2]
 
     currentdatetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    
+
     #load the raw json data into a python data object
     data = json.loads(jsonstr)
 
-    #pull the values that we are interested in 
+    #pull the values that we are interested in
     name=data['context']['dispatcher']['stores']['QuoteSummaryStore']['price']['shortName']
     price=data['context']['dispatcher']['stores']['QuoteSummaryStore']['price']['regularMarketPrice']['raw']
     volume=data['context']['dispatcher']['stores']['QuoteSummaryStore']['price']['regularMarketVolume']['raw']
-#     change=data['context']['dispatcher']['stores']['QuoteSummaryStore']['price']['regularMarketChange']['raw']
+    change=data['context']['dispatcher']['stores']['QuoteSummaryStore']['price']['regularMarketChange']['raw']
 
     #print the values
-#     print 'Symbol:', symbol
-#     print 'Name:', name
-#     print 'Price:', price
-#     print 'Volume:', volume
-#     print 'Change:', change
+    #print ('Symbol:', symbol)
+    #print 'Name:', name
+    #print 'Price:', price
+    #print 'Volume:', volume
+    #print 'Change:', change
 
-    
+
     res = []
     res.append(symbol)
     res.append(float(price))
     res.append(currentdatetime)
     res.append(int(volume))
-#     print res
+    #print res
     return res
 
 def getURL(stockCode):
     return "https://finance.yahoo.com/quote/"+ stockCode
 
-
-
-
+if __name__ == "__main__":
+    stock_info = getRealtimeStock("GOOG")
+    print stock_info[1]
